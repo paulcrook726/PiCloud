0aec33e865fc744b79fc716077e09213f8ae710c
Paul Crook <paulcrook726@gmail.com>
2015-10-24 20:25:58 +0200

    Base server commit

--- server_daemon.py
+++ server_daemon.py
@@ -1,16 +1,21 @@
 import socket
 
-class Socket:
+
+class ServerSocket:
     def __init__(self):
         self.socket = socket.socket()
         self.host = socket.gethostname()
         self.port = 6969
         self.socket.bind(self.host, self.port)
         self.socket.listen(5)
-
-    def run(self):
         while True:
             (conn_socket, address) = self.socket.accept()
-            print("Receiving connection from: " + address)
-            conn_socket.send(b"Connected to Picloud!")
+            byte = conn_socket.recv(1024)
+            with open("file.png", 'wb') as file:
+                with open("logfile.txt", 'wb') as logfile:
+                    while byte:
+                        log = "Receiving connection from: " + address
+                        file.write(bytes)
+                        byte = conn_socket.recv(1024)
+            print("Finished receiving data.")
             conn_socket.close()
