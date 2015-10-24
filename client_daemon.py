567fe45a784904479d1ca70518c27f9e24bb1d39
Paul Crook <paulcrook726@gmail.com>
2015-10-24 22:14:46 +0200

    Base server commit

--- client_daemon.py
+++ client_daemon.py
@@ -1,19 +1,17 @@
 import socket
 
 
-class ClientSocket:
-    def __init__(self):
-        self.host = socket.gethostname()
-        self.port = 6969
-        self.socket = socket.socket()
-        self.socket.connect((self.host, self.port))
-        with open("/home/paul/Music/Firecracker.mp3", 'rb') as file:
-            byte = file.read(1024)
-            while byte:
-                print("Sending")
-                self.socket.send(byte)
-                byte = file.read(1024)
-        print("Done!")
-        self.socket.close()
+host = socket.gethostname()
+port = 6962
+s = socket.socket()
 
-client = ClientSocket()
+
+s.connect((host, port))
+with open("/home/paul/text.txt", 'rb') as f:
+    byte = f.read(500000)
+    while byte:
+        s.send(byte)
+        print("Sending...")
+        byte = f.read(500000)
+print("Done!")
+s.close()
--- server_daemon.py
+++ server_daemon.py
@@ -1,26 +1,22 @@
 import socket
-import sys
 
 
-class ServerSocket:
-    def __init__(self):
-        self.socket = socket.socket()
-        self.host = socket.gethostname()
-        self.port = 6969
-        self.socket.bind((self.host, self.port))
-        self.socket.listen(5)
-        while True:
-            (conn_socket, address) = self.socket.accept()
-            byte = conn_socket.recv(1024)
-            with open("file.mp3", 'wb') as file:
-                with open("logfile.txt", 'wb') as logfile:
-                    while byte:
-                        log = "Receiving connection from: " + address
-                        file.write(bytes)
-                        byte = conn_socket.recv(1024)
-                        logfile.writelines(log)
-            print("Finished receiving data.")
-            conn_socket.close()
-            sys.exit(0)
+s = socket.socket()
+host = socket.gethostname()
+port = 6962
 
-server = ServerSocket()
+
+s.bind((host, port))
+s.listen(5)
+
+while True:
+    (conn_socket, address) = s.accept()
+    print("Received connection from" + str(address))
+    byte = conn_socket.recv(500000)
+    if not byte:
+        print("Done")
+        break
+    print("Receiving")
+    with open("file.txt", 'wb') as f:
+        f.write(byte)
+    conn_socket.close()
