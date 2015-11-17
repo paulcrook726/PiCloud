2e7be8d47a8b10f92d91380e111870c6571842fa
root <root@localhost.localdomain>
2015-11-17 14:16:26 +0100

    Directory restructure, inserted sphinx docs

---
+++ src/client.py
@@ -0,0 +1,12 @@
+from server import *
+
+def main():
+    logging.basicConfig(format='%(asctime)s %(message)s', filename='picloud.log', level=logging.INFO)
+    c = ClientSocket('192.168.2.194', 46000)
+    f = pre_proc('read.pdf')
+    send_file(c, f)
+    evaluate(c)
+
+
+if __name__ == '__main__':
+    main()
