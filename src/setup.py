947c72cb3af52f679e95a4611c9249804202f7e0
Paul Crook <paulcrook726@gmail.com>
2015-12-21 20:26:01 +0100

    update

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pInteServ',
-      version='0.122',
+      version='0.123',
       description='Module and cli for client/server directory sync.',
       long_description=readme(),
       url='https://github.com/paulcrook726/PiCloud.git',
